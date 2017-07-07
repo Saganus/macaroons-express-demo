0be1537f36c52b3a6e3a895bd3098182a2ba0e72
Akram Shehadi <akram.shehadi@gmail.com>
2017-07-07 03:56:16 -0500

    Set correct order for the verifier

--- app.js
+++ app.js
@@ -35,11 +35,10 @@ app.use(expressMongoDb('mongodb://localhost:27017/myproject'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(getMacaroonSecret({collection: "ACEs"}));
 app.use(mAuthVerifier({serverId : serverId, publicScope : publicScope}));
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
 app.use("/users", users);
